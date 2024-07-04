e294a82b1ba383e61a6b7b4c697ea16b85611a43
rodrigomalagon <rodrigo.malagon.rodriguez@gmail.com>
2024-07-04 17:27:16 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' lorem example text data
+#'
+#' A text snippet created using the 'stringi::stri_rand_lipsum()' and a few lines
+#' of string processing
+#'
+#' @format ## `lorem`
+#' A 'lorem ipsum' text of ten joined paragraphs created using 'stringi' package
+"lorem"
